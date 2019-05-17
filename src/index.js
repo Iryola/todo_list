a86c59bfb2d69de85e5cc0416f1f258bf50ae8fc
Iryola <adamfam@hotmail.com>
2019-05-17 15:04:57 -0600

    Brought my custom built python API in with the React front end.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
 import TodoItem from "./todo-items";
 import "./styles.css";
@@ -14,6 +15,15 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch("http://localhost:5000/todos")
+    .then(response => response.json())
+    .then(data =>
+      this.setState({
+        todos: data
+      }))
+  }
+
   onChange = event => {
     this.setState({
       todo: event.target.value
@@ -22,17 +32,50 @@ class App extends React.Component {
 
   renderTodos = () => {
     return this.state.todos.map(item => {
-      return <TodoItem title={item} />;
+      return (
+      <TodoItem 
+      key={item[0]}
+      id={item[0]}
+      title={item[1]}
+      done={item[2]} 
+      deleteItem={this.deleteItem}
+      />
+      );
     });
   };
 
   addTodo = event => {
     event.preventDefault();
-    this.setState({
-      todos: [...this.state.todos, this.state.todo],
-      todo: ""
-    });
+    axios({
+      method: "post",
+      url: "http://localhost:5000/add-todo",
+      headers: { "content-type": "application/json" },
+      data: {
+        title: this.state.todo,
+        done: false
+      }
+    })
+    .then(data => {
+      this.setState({
+        todos: [...this.state.todos, data.data],
+        todo: ""
+      })
+    })
+    .catch(error => console.log(error));
   };
+
+  deleteItem = id => {
+    fetch(`http://localhost:5000/todo/${id}`, {
+      method: "DELETE"
+    })
+    .then(this.setState({
+      todos: this.state.todos.filter(item => {
+        return item[0] !== id
+      })
+    }))
+    .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="app">
--- src/todo-items.js
+++ src/todo-items.js
@@ -5,8 +5,23 @@ class TodoItem extends React.Component {
     super(props);
 
     this.state = {
-      done: false
+      done: props.done
     };
+  }
+
+  toggleDone = () => {
+    fetch(`http://localhost:5000/todo/${this.props.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: this.props.title,
+        done: !this.state.done
+      })
+    })
+    .then(this.setState({
+      done: !this.state.done
+    }))
+    .catch(error => console.log(error))
   }
 
   render() {
@@ -14,9 +29,11 @@ class TodoItem extends React.Component {
       <div className="todo-item">
         <input
           type="checkbox"
-          onClick={() => this.setState({ done: !this.state.done })}
+          onClick={this.toggleDone}
+          defaultChecked={this.state.done}
         />
         <p className={this.state.done ? "done" : null}>{this.props.title}</p>
+        <button onClick={() => this.props.deleteItem(this.props.id)}>X</button>
       </div>
     );
   }
